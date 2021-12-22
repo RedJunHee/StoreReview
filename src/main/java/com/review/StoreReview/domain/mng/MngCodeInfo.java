9122458048fd677cdc8f0245c1f89cca75d67679
조준희 <46863381+RedJunHee@users.noreply.github.com>
2021-12-22 14:08:38 +0900

    model : MngCode, MngCodeInfo, User 생성
    model : MngCode, MngCodeInfo, User 생성

---
+++ src/main/java/com/review/StoreReview/domain/mng/MngCodeInfo.java
@@ -0,0 +1,54 @@
+package com.review.StoreReview.domain.mng;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+
+
+// 객체   : MngCodeInfo (Model)
+// 설명   : MNG_DB.MngCodeInfo 테이블에 매핑될 Model
+// 이름   : 조 준 희
+// 생성   : 2021-12-21
+// 기록   : [2021-12-21] - TEMP History/
+
+
+public class MngCodeInfo {
+
+    // 1. id 컬럼
+    // 2. 테이블과 매핑될 컬럼 = "CODE_ID"
+    // 3. GenerationType.IDENTITY DBMS에게 키 생성 권한을 맡김. /
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "SEQ")
+    private Integer SEA;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "CODE_ID")
+    private MngCode CODE_ID;
+
+    // 1. 테이블과 매핑될 컬럼 = "CODE_NAME"/
+    @Column(name = "CODE_NAME", nullable = false, length=100)
+    private String CODE_NAME;
+
+    // 1. 테이블과 매핑될 컬럼 = "DESC"/
+    @Column(name = "DESC", nullable = false, length=100, columnDefinition = "VARCHAR(100) Default ''")
+    private String DESC;
+
+    @Column(name = "SORT_ORDER", nullable = false, columnDefinition = "INTEGER Default 0")
+    private Integer SORT_ORDER;
+
+    // 1. 테이블과 매핑될 컬럼 = "USE_YN"/
+    @Column(name = "USE_YN", nullable = false, columnDefinition = "CHAR(1) Default 'N'")
+    private char USE_YN;
+
+    @Builder
+    public MngCodeInfo(Integer SEA, MngCode CODE_ID, String CODE_NAME, String DESC, Integer SORT_ORDER, char USE_YN) {
+        this.SEA = SEA;
+        this.CODE_ID = CODE_ID;
+        this.CODE_NAME = CODE_NAME;
+        this.DESC = DESC;
+        this.SORT_ORDER = SORT_ORDER;
+        this.USE_YN = USE_YN;
+    }
+
+}
--- src/main/java/com/review/StoreReview/domain/user/User.java
+++ src/main/java/com/review/StoreReview/domain/user/User.java
@@ -1,15 +1,66 @@
 package com.review.StoreReview.domain.user;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.*;
 
+// 객체   : USER (Model)
+// 설명   : CUST_DB.USER_INFO 테이블에 매핑될 Model
+// 이름   : 조 준 희
+// 생성   : 2021-12-21
+// 기록   : [2021-12-21] - TEMP History/
+
 @Entity
+@Getter
+@Table(name= "USER_INFO")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-    // 다른 객체에서 필요하여 먼저 선언했습니다.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
-    private Long id;
+    @Column(name="SUID", nullable = false)
+    private String SUID;
 
+    @Column(name="SAID", nullable = false)
+    private String SAID;
+
+    @Column(name="ID", nullable = false)
+    private String ID;
+
+    @Column(name="PASSWORD", nullable = false)
+    private String PASSWORD;
+
+    @Column(name="NAME", nullable = false)
+    private String NAME;
+
+    @Column(name="NICKNAME", nullable = false)
+    private String NICKNAME;
+
+    @Column(name="BIRTH_DATE", nullable = false)
+    private String BIRTH_DATE;
+
+    @Column(name="GENDER", nullable = false)
+    private String GENDER;
+
+    @Column(name="PHONE", nullable = false)
+    private String PHONE;
+
+    @Builder
+    public User(String SUID, String SAID, String ID, String PASSWORD
+            , String NAME, String NICKNAME, String BIRTH_DATE
+            , String GENDER, String PHONE) {
+        this.SUID = SUID;
+        this.SAID = SAID;
+        this.ID = ID;
+        this.PASSWORD = PASSWORD;
+        this.NAME = NAME;
+        this.NICKNAME = NICKNAME;
+        this.BIRTH_DATE = BIRTH_DATE;
+        this.GENDER = GENDER;
+        this.PHONE = PHONE;
+    }
 }
