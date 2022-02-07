7165ecdcef9e9178bd4af8edf1ede2f0b9c68952
moonz <banan99@naver.com>
2022-02-07 16:57:33 +0900

    Update S3Service.java

--- src/main/java/com/review/storereview/service/S3Service.java
+++ src/main/java/com/review/storereview/service/S3Service.java
@@ -3,7 +3,6 @@ package com.review.storereview.service;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
